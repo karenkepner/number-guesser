f5581e8693f62b28eaa36d601154000364459591
Karen Kepner <kkepner14@gmail.com>
2017-02-01 08:56:19 -0700

    added feature to show the last guess

--- js/main.js
+++ js/main.js
@@ -1,16 +1,50 @@
-var answer = Math.floor(Math.random() * 10);
 
-var playerGuess = document.querySelector('.numberGuess')
+//create a variable to store the answer
+var answer;
 
-playerGuess.addEventListener("click", function(){
-  var h2 = document.querySelector(".feedback");
-    if (playerGuess > answer){
+//create a variable for each button/input
+var numberGuess = document.querySelector('.number-guess')
+var guessInput = document.querySelector('.guess-input');
+var clearInput = document.querySelector('.clear-input');
+var reset = document.querySelector('.reset');
+//create other cool variables
+var bigLastGuess = document.querySelector('.big-last-guess')
+
+//these are for adding the range
+var minGuess = 1;
+var maxGuess = 100;
+
+//generate the random number for the player to guess
+function generateNumber( ) {
+  answer = Math.floor((Math.random() * 100) + 1);
+}
+//this triggers the random number to generate on page load
+window.onload = function() {
+  generateNumber();
+}
+
+//evaluate the players guess
+var playerGuess = document.querySelector('.number-guess')
+
+guessInput.addEventListener("click", function(){
+    var h2 = document.querySelector(".feedback");
+    if (playerGuess.value > answer){
       h2.innerText =  "That is too high.";
-    } else if (playerGuess < answer) {
+    } else if (playerGuess.value < answer) {
       h2.innerText = "That is too low.";
-    } else if (playerGuess == answer) {
+    } else if (playerGuess.value == answer) {
       h2.innerText = "BOOM!!!";
     } else {
       h2.innerText = "Your guess must be a number in the correct range.";
     };
+})
+//clear the last guess
+clearInput.addEventListener('click', function (){
+
+})
+
+//display the players' last guess
+guessInput.addEventListener("click", function(){
+  var displayGuess = document.querySelector('.big-last-guess');
+  displayGuess.innerText = playerGuess.value;
 })
